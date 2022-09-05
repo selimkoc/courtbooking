7268d283ba040089656c90de8db94a81aed48eef
Selim Koç <selimkoc@gmail.com>
2022-09-05 16:54:05 +0300

    Create archieve-gamelogs.php

---
+++ archieve-gamelogs.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+get_header();
+?>
+
+<main id="site-content">
+
+
+		<?php
+
+
+	if ( have_posts() ) {
+
+
+		while ( have_posts() ) {
+
+			the_post();
+
+			the_meta();
+			echo '<br><br>';
+
+		}
+	}
+	?>
+
+	<?php get_template_part( 'template-parts/pagination' ); ?>
+
+</main><!-- #site-content -->
+
+
+<?php
+get_footer();
